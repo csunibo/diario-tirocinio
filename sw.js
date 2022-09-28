77558884d35d173b9764cf914284ba9ece269b6c
kocierik <kocierik@gmail.com>
2022-09-28 15:30:23 +0200

    chore: isOk?

--- sw.js
+++ sw.js
@@ -1,15 +1,48 @@
-// Choose a different app prefix name
-var APP_PREFIX = 'pwa_';
-
-// The version of the cache. Every time you change any of the files
-// you need to change this version (version_01, version_02…). 
-// If you don't change the version, the service worker will give your
-// users the old files!
-var VERSION = 'version_02';
-
-// The files to make available for offline use. make sure to add 
-// others to this list
+var APP_PREFIX = 'dicethrow_';
+var VERSION = 'version_06';
+var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [
   '/oreTirocinio/',
-  '/oreTirocinio/index.html',
+  '/oreTirocinio/index.html'
 ]
+
+self.addEventListener('fetch', function(e) {
+  console.log('fetch request : ' + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(request) {
+      if (request) {
+        console.log('responding with cache : ' + e.request.url);
+        return request
+      } else {
+        console.log('file is not cached, fetching : ' + e.request.url);
+        return fetch(e.request)
+      }
+    })
+  )
+})
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log('installing cache : ' + CACHE_NAME);
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      var cacheWhitelist = keyList.filter(function(key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      cacheWhitelist.push(CACHE_NAME);
+      return Promise.all(keyList.map(function(key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i]);
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
