aa12082c067354b76da74b0ba07059fedad7db93
Samuele Musiani <samu@teapot.ovh>
2024-06-16 20:26:38 +0200

    red when wrong submit

--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -6,18 +6,20 @@ export default defineComponent({
     return {
       tmpTitle: '',
       tmpTime: 1,
-      tmpDescription: ''
+      tmpDescription: '',
+      submittedOnce: false
     }
   },
   emits: ['addActivity'],
   methods: {
     handleSubmit(this: any) {
       // Should check for errors :)
-
       this.$emit('addActivity', this.tmpTitle, this.tmpTime, this.tmpDescription)
       this.tmpTitle = ''
       this.tmpTime = 1
       this.tmpDescription = ''
+
+      this.submittedOnce = false
     }
   }
 })
@@ -32,13 +34,24 @@ export default defineComponent({
           v-model="tmpTitle"
           id="title"
           type="text"
+          required
           class="rounded border p-2 bg-sky-100 text-black"
+          :class="{
+            'invalid:bg-red-100': submittedOnce,
+            'invalid:border-red-200': submittedOnce,
+            'invalid:border': submittedOnce
+          }"
         />
       </div>
 
       <div class="mt-5">
         <label>Ore:</label>
-        <input v-model="tmpTime" type="number" class="rounded border p-2 bg-sky-100 text-black" />
+        <input
+          v-model="tmpTime"
+          type="number"
+          min="1"
+          class="rounded border p-2 bg-sky-100 text-black invalid:border-red-200 invalid:border invalid:bg-red-100"
+        />
       </div>
 
       <div class="mt-5">
@@ -48,12 +61,18 @@ export default defineComponent({
           placeholder="Add description..."
           required
           class="block h-32 w-full rounded border p-2 outline-none bg-sky-100 text-black"
+          :class="{
+            'invalid:bg-red-100': submittedOnce,
+            'invalid:border-red-200': submittedOnce,
+            'invalid:border': submittedOnce
+          }"
         ></textarea>
       </div>
 
       <div class="flex justify-center">
         <button
           class="m-5 w-20 rounded bg-blue-400 p-2 border-0 text-gray-50 hover:bg-blue-500 hover:text-white"
+          @click="submittedOnce = true"
         >
           Add
         </button>
