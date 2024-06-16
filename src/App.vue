9cdf052c9a872c6a569366cf755c3143f122e93c
Samuele Musiani <samu@teapot.ovh>
2024-06-16 15:31:03 +0200

    tailwind + base form

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
+import InputForm from './components/InputForm.vue'
 </script>
 
 <template>
-  Diario tirocinio
+  <div class="flex justify-center">
+    <div class="w-[40em] mt-10">
+      <h1 class="font-bold text-2xl p-4 text-center mb-10">Diario tirocinio</h1>
+      <InputForm />
+    </div>
+  </div>
 </template>
 
 <style></style>
---
+++ src/components/InputForm.vue
@@ -0,0 +1,41 @@
+<script></script>
+
+<template>
+  <div class="p-2">
+    <form>
+      <div class="mt-5">
+        <label for="title">Titolo: </label>
+        <input id="title" type="text" class="rounded border p-2" />
+      </div>
+
+      <div class="mt-5">
+        <label>Ore:</label>
+        <input type="number" class="rounded border p-2" />
+      </div>
+
+      <div class="mt-5">
+        <label for="description">Descrizione:</label>
+        <textarea class="block w-full border h-32 p-2 outline-none rounded"></textarea>
+      </div>
+
+      <div class="flex justify-center">
+        <button class="m-5 border rounded p-2 w-20 bg-blue-300">Add</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+input {
+  /*border-bottom: thin solid gray;*/
+  display: block;
+  width: 100%;
+  outline: none;
+}
+
+label {
+  display: block;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+</style>
