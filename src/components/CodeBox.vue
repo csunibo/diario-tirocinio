495ca1533c200aa7dbde1bc3c547c09fd57830d7
Samuele Musiani <samu@teapot.ovh>
2024-06-18 10:40:55 +0200

    simple CodeBlock

---
+++ src/components/CodeBox.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
+
+<template>
+  <div class="border border-blue-500 rounded bg-blue-500 p-4">
+    <div class="whitespace-pre-wrap" style="tab-size: 2">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style></style>
--- src/components/ConvertionWindow.vue
+++ src/components/ConvertionWindow.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { defineComponent, type PropType } from 'vue'
 import type { Activity } from '@/types'
+import CodeBox from './CodeBox.vue'
 
 export default defineComponent({
   props: {
@@ -11,16 +12,21 @@ export default defineComponent({
   },
   methods: {
     toSimpleJson(): string {
-      return 'WEEEEEEE'
+      return JSON.stringify(this.activities, ['Title', 'Duration', 'Description'], '\t')
     }
-  }
+  },
+  components: { CodeBox }
 })
 </script>
 
 <template>
   <div class="h-full">
-    {{ activities }}
-    {{ toSimpleJson() }}
+    <div class="bg-csunibo-light-blu rounded-b-lg p-12">
+      <h1 class="font-bold m-b-2 text-lg mb-2">JSON:</h1>
+      <CodeBox>
+        {{ toSimpleJson() }}
+      </CodeBox>
+    </div>
   </div>
 </template>
 
