d55d79f9a7e1384e5e068bbcb678461b30bb57cc
Samuele Musiani <samu@teapot.ovh>
2024-06-19 00:14:51 +0200

    wip side menus

---
+++ src/components/InfoMenu.vue
@@ -0,0 +1,48 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  emits: ['close']
+})
+</script>
+
+<template>
+  <div class="fixed top-0 left-0 w-full h-full">
+    <div @click="$emit('close')" class="w-full h-full">
+      <div
+        @click.stop=""
+        class="bg-csunibo-light-blu w-80 h-full shadow-2xl border-black flex flex-col justify-between"
+      >
+        <div class="pl-5 pr-5">
+          <h1 class="font-bold text-center text-xl p-5 border-b border-b-white">INFO</h1>
+        </div>
+        <div>
+          <p class="m-5 p-5 bg-blue-500/[0.8] rounded-lg">
+            <b>NOTA:</b> Il PDF ottenuto non è un documento ufficiale valido, ma una bozza utile del
+            lavoro svolto da trascrivere successivamente sul documento ufficiale fornitosi dalla
+            piattaforma tirocini unibo.
+          </p>
+        </div>
+        <div>
+          <a
+            href="https://csunibo.students.cs.unibo.it/wiki/applicativi-web/diario-tirocinio/index.html"
+            class="block p-5 border-blue-500 border text-center m-5 text-blue-500 rounded hover:bg-blue-400 hover:text-white hover:border-blue-400"
+            >Documentazione</a
+          >
+          <a
+            href="https://csunibo.students.cs.unibo.it"
+            class="block p-5 border-blue-500 border text-center m-5 text-blue-500 rounded hover:bg-blue-400 hover:text-white hover:border-blue-400"
+            >CSUnibo</a
+          >
+          <a
+            href="https://github.com/csunibo/diario-tirocinio"
+            class="block p-5 border-blue-500 border text-center m-5 text-blue-500 rounded hover:bg-blue-400 hover:text-white hover:border-blue-400"
+            >Contribuisci</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
---
+++ src/components/SettingsMenu.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      settings: {
+        // Da mettere un tipo globale e farselo passare dal genitore
+        // per le proprietà già inserite
+        orePulsanti: false
+      }
+    }
+  },
+  emits: ['close']
+})
+</script>
+
+<template>
+  <div class="fixed top-0 right-0 w-full h-full">
+    <div @click="$emit('close')" class="w-full h-full">
+      <div
+        @click.stop=""
+        class="bg-csunibo-light-blu w-80 h-full shadow-2xl border-black float-right"
+      >
+        <div class="pl-5 pr-5 mb-10">
+          <h1 class="font-bold text-center text-xl p-5 border-b-white border-b">SETTINGS</h1>
+        </div>
+        <div class="p-5">
+          <label class="inline-flex items-center cursor-pointer">
+            <input type="checkbox" :value="settings.orePulsanti" class="sr-only peer" />
+            <div
+              class="relative w-11 h-6 bg-gray-700 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"
+            ></div>
+            <span class="ms-3 text-sm font-medium">Ore a pulsanti</span>
+          </label>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
